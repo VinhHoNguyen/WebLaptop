4ad0d95fe4e9d7c9cb6f6694221923e0d08b42a2
VinhHoNguyen <vinhmatlo432@gmail.com>
2026-05-20 02:14:43 +0700

    fix n8n: chown /home/node to fix .cache permission denied

--- services/n8n/entrypoint.sh
+++ services/n8n/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir -p /home/node/.n8n
-chown -R node:node /home/node/.n8n
+mkdir -p /home/node/.n8n /home/node/.cache
+chown -R node:node /home/node
 exec su -s /bin/sh -c "exec n8n" node
